e609beb5061f979da69f22ef05e28bc6e6209633
je4npw <je4n.pw@gmail.com>
2024-09-04 15:19:04 -0300

    5 - teste upgrade depois deploy

---
+++ app/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <footer class="bg-rt-green text-white py-4">
+            <div class="container mx-auto text-center">
+                &copy; 2024 Rede Salomão. Todos os direitos reservados.
+            </div>
+        </footer>
+    );
+}
--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import HomeSection from "@/app/components/HomeSection";
 import SobreSection from "@/app/components/SobreSection";
 import ServicesSection from "@/app/components/ServicesSection";
 import ContactSection from "@/app/components/Contactsession";
+import Footer from "@/app/components/Footer";
 
 export default function Home() {
     return (
@@ -12,6 +13,7 @@ export default function Home() {
             <SobreSection />
             <ServicesSection />
             <ContactSection />
+            <Footer />
         </>
     );
 }
