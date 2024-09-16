326eb4eec3527ed0c6ee5701b695113ca665354c
je4npw <je4n.pw@gmail.com>
2024-09-16 15:57:20 -0300

    bulk build deploy

--- app/page.js
+++ app/page.js
@@ -58,7 +58,6 @@ export default function Home() {
             <ServicesSection/>
             <AtendidasSection />
             <EstruturaSection/>
-            {/*<ContactSection/>*/}
             <WhatsAppButton/>
             <Footer/>
         </>
