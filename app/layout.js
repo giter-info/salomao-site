6b192ea9990a47fe3cfeb6b4b0906ab135ac4731
je4npw <je4n.pw@gmail.com>
2024-09-16 19:52:49 -0300

    bulk build deploy 5

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,53 @@
-import {Inter} from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({subsets: ["latin"]});
+const inter = Inter({ subsets: ["latin"] });
 
-export default function RootLayout({children}) {
-    return (<html lang="en">
-    <body className={inter.className}>
-        <div className="bg-rt-green">
-            {children}
-        </div>
-    </body>
-    </html>);
+const commonTitle = "Residencial Terapêutico Salomão";
+const commonDescription =
+    "Residência para portadores de transtornos mentais, egressos de internações psiquiátricas e hospitais de custódia.";
+const commonUrl = "https://redesalomao.com.br/";
+const commonImage = "https://redesalomao.com.br/capa.jpg";
+const commonImageDetails = {
+    url: commonImage,
+    width: 1200,
+    height: 630,
+    alt: "Imagem de prévia do site.",
+};
+
+export const metadata = {
+    title: commonTitle,
+    description: commonDescription,
+    openGraph: {
+        type: "website",
+        title: commonTitle,
+        description: commonDescription,
+        url: commonUrl, // Pode não ser necessário para OpenGraph
+        images: [commonImageDetails],
+        locale: "pt_BR",
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: commonTitle,
+        description: commonDescription,
+        images: [commonImage], // Corrigido para plural
+        image_alt: "Imagem de prévia do site no Twitter", // Adicionado alt
+    },
+    author: "Je4nPw",
+    keywords: [
+        "Residencial Terapêutico",
+        "Transtornos Mentais",
+        "Residencial",
+        "desospitalização",
+    ],
+};
+
+export default function RootLayout({ children }) {
+    return (
+        <html lang="pt">
+        <body className={inter.className}>
+        <div className="bg-rt-green">{children}</div>
+        </body>
+        </html>
+    );
 }
--- app/page.js
+++ app/page.js
@@ -9,45 +9,6 @@ import EstruturaSection from "@/app/components/EstruturaSection";
 import WhatsAppButton from "@/app/components/WhatsAppButton";
 import AtendidasSection from "@/app/components/AtendidasSection";
 
-const commonTitle = "Residencial Terapêutico Salomão";
-const commonDescription = "Residência para portadores de transtornos mentais, egressos de internações psiquiátricas e hospitais de custódia.";
-const commonUrl = "https://redesalomao.com.br/";
-const commonImage = "https://redesalomao.com.br/capa.jpg";
-const commonImageDetails = {
-    url: commonImage,
-    width: 600,
-    height: 600,
-    alt: "Imagem de prévia do site.",
-};
-
-export const metadata = {
-    title: commonTitle,
-    description: commonDescription,
-    url: commonUrl,
-    openGraph: {
-        type: "website",
-        title: commonTitle,
-        description: commonDescription,
-        url: commonUrl,
-        images: [commonImageDetails],
-        locale: "pt_BR",
-    },
-    twitter: {
-        card: "summary_large_image",
-        title: commonTitle,
-        description: commonDescription,
-        url: commonUrl,
-        image: commonImage,
-    },
-    whatsapp: {
-        title: commonTitle,
-        description: commonDescription,
-        image: commonImage,
-    },
-    author: "Je4nPw",
-    keywords: ["Residencial Terapêutico", "Transtornos Mentais", "Residencial", "desospitalização"],
-};
-
 export default function Home() {
     return (
         <>
