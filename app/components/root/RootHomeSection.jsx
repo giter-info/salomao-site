6e3f23d4c6767a30eb75addc9716d0a04ca6aadc
je4npw <je4n.pw@gmail.com>
2025-09-02 19:14:12 -0300

    2025-09-02.2 - Aumentando o site

--- app/components/root/RootHomeSection.jsx
+++ app/components/root/RootHomeSection.jsx
@@ -6,7 +6,7 @@ import * as motion from "framer-motion/client";
 
 export default function RootHomeSection() {
     return (
-        <section id="home" className="bg-hero bg-cover bg-no-repeat bg-center min-h-screen flex items-center bg-white">
+        <section id="home" className="bg-root bg-cover bg-no-repeat bg-center min-h-screen flex items-center bg-white">
             <div className="relative isolate px-6 pt-14 lg:px-8 w-full">
                 <div className="mx-auto max-w-2xl py-10 sm:py-12">
                     <div className="sm:mb-8 flex justify-center">
@@ -44,7 +44,7 @@ export default function RootHomeSection() {
                             initial={{opacity: 0, y: -20}}
                             animate={{opacity: 1, y: 0}}
                             transition={{duration: 1, delay: 0.2, ease: "easeOut"}}
-                            className="text-2xl xl:text-6xl font-bold text-slate-800"
+                            className="text-2xl xl:text-6xl font-bold text-white"
                         >
                             Respeitando o normal
                         </motion.h1>
@@ -52,7 +52,7 @@ export default function RootHomeSection() {
                             initial={{opacity: 0, y: -20}}
                             animate={{opacity: 1, y: 0}}
                             transition={{duration: 1, delay: 0.2, ease: "easeOut"}}
-                            className="text24xl xl:text-6xl font-bold text-slate-800 mb-4"
+                            className="text24xl xl:text-6xl font-bold text-white mb-4"
                         >
                             de cada um!
                         </motion.h1>
@@ -61,7 +61,7 @@ export default function RootHomeSection() {
                             initial={{opacity: 0, y: -20}}
                             animate={{opacity: 1, y: 0}}
                             transition={{duration: 1, delay: 0.4, ease: "easeOut"}}
-                            className="text-lg font-bold text-slate-800 mb-4"
+                            className="text-lg font-bold text-white mb-4"
                         >
                             Bem-vindo à Rede Salomão, criada para oferecer serviços acolhedores inclusivos e qualificados.
                         </motion.h2>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,30 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'hero': "url('/bg-main.png')",
-        'hero2': "url('/bg-2.png')",
-      },
-      colors: {
-        "rt-primary": "#85F2CA",
-        "rt-info": "#BDF2D9",
-        "rt-green": "#1B402B",
-        "rt-white": "#F2F0EB",
-        "rt-dark": "#0D0D0D",
-      },
-      fontFamily:{
-        script: ['Lucien Schoenschriftv CAT', 'cursive'],
-        mont: ['Montserrat', 'sans-serif'],
-        cardo: ['Delius', 'sans-serif'],
-      },
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                "root": "url('/bg-root.png')",
+                "hero": "url('/bg-main.png')",
+                "hero2": "url('/bg-2.png')",
+            },
+            colors: {
+                "rt-primary": "#85F2CA",
+                "rt-info": "#BDF2D9",
+                "rt-green": "#1B402B",
+                "rt-white": "#F2F0EB",
+                "rt-dark": "#0D0D0D",
+            },
+            fontFamily: {
+                script: ["Lucien Schoenschriftv CAT", "cursive"],
+                mont: ["Montserrat", "sans-serif"],
+                cardo: ["Delius", "sans-serif"],
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
