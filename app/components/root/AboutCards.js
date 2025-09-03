3a2793f59f91919d2de01bc89d001fae6e3a2f51
je4npw <je4n.pw@gmail.com>
2025-09-03 16:13:22 -0300

    2025-09-03.1 - Aumentando o site

--- app/components/root/AboutCards.js
+++ app/components/root/AboutCards.js
@@ -1,61 +1,61 @@
 export default function aboutCards() {
     return (
-        <section class="text-gray-400 bg-slate-200 body-font">
-            <div class="container px-5 py-24 mx-auto">
-                <h1 class="sm:text-3xl text-2xl font-medium title-font text-center text-slate-800 mb-20">“Somos uma rede
+        <section className="text-gray-400 bg-slate-200 body-font">
+            <div className="container px-5 py-24 mx-auto">
+                <h1 className="sm:text-3xl text-2xl font-medium title-font text-center text-slate-800 mb-20">“Somos uma rede
                     comprometida com o bem-estar, a inclusão
-                    <span class="hidden sm:block"> e a construção de vínculos humanos sólidos.”</span>
+                    <span className="hidden sm:block"> e a construção de vínculos humanos sólidos.”</span>
                 </h1>
-                <div class="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6">
-                    <div class="p-4 md:w-1/3 flex">
+                <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6">
+                    <div className="p-4 md:w-1/3 flex">
                         <div
-                            class="w-12 h-12 inline-flex items-center justify-center rounded-full bg-gray-800 text-rt-primary mb-4 flex-shrink-0">
+                            className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-gray-800 text-rt-primary mb-4 flex-shrink-0">
                             <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path
                                     d="M19 9.77806V16.2C19 17.8801 19 18.7202 18.673 19.3619C18.3854 19.9264 17.9265 20.3854 17.362 20.673C16.7202 21 15.8802 21 14.2 21H9.8C8.11984 21 7.27976 21 6.63803 20.673C6.07354 20.3854 5.6146 19.9264 5.32698 19.3619C5 18.7202 5 17.8801 5 16.2V9.7774M21 12L15.5668 5.96393C14.3311 4.59116 13.7133 3.90478 12.9856 3.65138C12.3466 3.42882 11.651 3.42887 11.0119 3.65153C10.2843 3.90503 9.66661 4.59151 8.43114 5.96446L3 12M14 12C14 13.1045 13.1046 14 12 14C10.8954 14 10 13.1045 10 12C10 10.8954 10.8954 9.99996 12 9.99996C13.1046 9.99996 14 10.8954 14 12Z"
-                                    stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                    stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                             </svg>
                         </div>
-                        <div class="flex-grow pl-6">
-                            <h2 class="text-slate-900 text-lg title-font font-medium mb-2">Residenciais
+                        <div className="flex-grow pl-6">
+                            <h2 className="text-slate-900 text-lg title-font font-medium mb-2">Residenciais
                                 Terapêuticos</h2>
-                            <p class="leading-relaxed text-slate-800">Proporcionamos ambientes residenciais terapêuticos
+                            <p className="leading-relaxed text-slate-800">Proporcionamos ambientes residenciais terapêuticos
                                 estruturados, fundamentados no cuidado integral, no respeito à dignidade humana e no
                                 compromisso com a qualidade de vida. Uma trajetória que reafirma diariamente a
                                 importância da inclusão social, da construção de vínculos significativos e do
                                 desenvolvimento de um espaço seguro e acolhedor para todos.</p>
                         </div>
                     </div>
-                    <div class="p-4 md:w-1/3 flex">
+                    <div className="p-4 md:w-1/3 flex">
                         <div
-                            class="w-12 h-12 inline-flex items-center justify-center rounded-full bg-gray-800 text-rt-primary mb-4 flex-shrink-0">
+                            className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-gray-800 text-rt-primary mb-4 flex-shrink-0">
                             <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path
                                     d="M19 9.77806V16.2C19 17.8801 19 18.7202 18.673 19.3619C18.3854 19.9264 17.9265 20.3854 17.362 20.673C16.7202 21 15.8802 21 14.2 21H9.8C8.11984 21 7.27976 21 6.63803 20.673C6.07354 20.3854 5.6146 19.9264 5.32698 19.3619C5 18.7202 5 17.8801 5 16.2V9.7774M21 12L15.5668 5.96393C14.3311 4.59116 13.7133 3.90478 12.9856 3.65138C12.3466 3.42882 11.651 3.42887 11.0119 3.65153C10.2843 3.90503 9.66661 4.59151 8.43114 5.96446L3 12M14 12C14 13.1045 13.1046 14 12 14C10.8954 14 10 13.1045 10 12C10 10.8954 10.8954 9.99996 12 9.99996C13.1046 9.99996 14 10.8954 14 12Z"
-                                    stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                    stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                             </svg>
                         </div>
-                        <div class="flex-grow pl-6">
-                            <h2 class="text-slate-900 text-lg title-font font-medium mb-2">Residenciais Inclusivos</h2>
-                            <p class="leading-relaxed text-slate-800">Oferecemos residenciais inclusivos voltados para
+                        <div className="flex-grow pl-6">
+                            <h2 className="text-slate-900 text-lg title-font font-medium mb-2">Residenciais Inclusivos</h2>
+                            <p className="leading-relaxed text-slate-800">Oferecemos residenciais inclusivos voltados para
                                 pessoas com deficiência, promovendo ambientes acessíveis e acolhedores que estimulam a
                                 autonomia, fortalecem a convivência comunitária e asseguram a valorização da diversidade
                                 humana. Nosso propósito é garantir que cada pessoa tenha a oportunidade de viver com
                                 dignidade, respeito e plena participação social.</p>
                         </div>
                     </div>
-                    <div class="p-4 md:w-1/3 flex">
+                    <div className="p-4 md:w-1/3 flex">
                         <div
-                            class="w-12 h-12 inline-flex items-center justify-center rounded-full bg-gray-800 text-rt-primary mb-4 flex-shrink-0">
+                            className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-gray-800 text-rt-primary mb-4 flex-shrink-0">
                             <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path
                                     d="M19 9.77806V16.2C19 17.8801 19 18.7202 18.673 19.3619C18.3854 19.9264 17.9265 20.3854 17.362 20.673C16.7202 21 15.8802 21 14.2 21H9.8C8.11984 21 7.27976 21 6.63803 20.673C6.07354 20.3854 5.6146 19.9264 5.32698 19.3619C5 18.7202 5 17.8801 5 16.2V9.7774M21 12L15.5668 5.96393C14.3311 4.59116 13.7133 3.90478 12.9856 3.65138C12.3466 3.42882 11.651 3.42887 11.0119 3.65153C10.2843 3.90503 9.66661 4.59151 8.43114 5.96446L3 12M14 12C14 13.1045 13.1046 14 12 14C10.8954 14 10 13.1045 10 12C10 10.8954 10.8954 9.99996 12 9.99996C13.1046 9.99996 14 10.8954 14 12Z"
-                                    stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                    stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                             </svg>
                         </div>
-                        <div class="flex-grow pl-6">
-                            <h2 class="text-slate-900 text-lg title-font font-medium mb-2">ILPIs</h2>
-                            <p class="leading-relaxed text-slate-800">Em breve vamos oferecer Instituições de Longa
+                        <div className="flex-grow pl-6">
+                            <h2 className="text-slate-900 text-lg title-font font-medium mb-2">ILPIs</h2>
+                            <p className="leading-relaxed text-slate-800">Em breve vamos oferecer Instituições de Longa
                                 Permanência que asseguram um domicílio coletivo para pessoas idosas, pautado na
                                 liberdade, na dignidade e na cidadania, promovendo ambientes de cuidado contínuo,
                                 respeito e valorização da experiência de vida</p>
